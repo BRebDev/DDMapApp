47c50ac7d298160000243681e93e21cc4379dbc6
BRebDev <b.raskin03@gmail.com>
2024-04-26 18:47:48 +0200

    Added Assets folder

---
+++ Assets/OBJImport/Samples/ObjFromStream.cs
@@ -0,0 +1,22 @@
+﻿using Dummiesman;
+using System.IO;
+using System.Text;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class ObjFromStream : MonoBehaviour {
+	
+
+void Start () {
+        //make UnityWebRequest
+        var request = UnityWebRequest.Get("https://people.sc.fsu.edu/~jburkardt/data/obj/lamp.obj");
+        request.SendWebRequest();
+        while (!request.isDone)
+            System.Threading.Thread.Sleep(1);
+        
+        //create stream and load
+        var textStream = new MemoryStream(Encoding.UTF8.GetBytes(request.downloadHandler.text));
+        var loadedObj = new OBJLoader().Load(textStream);
+    }
+
+}
